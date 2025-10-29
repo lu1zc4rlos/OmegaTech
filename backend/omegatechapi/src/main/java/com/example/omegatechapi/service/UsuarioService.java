64992132790ac273046e9cc526dde1d2cf5de386
lu1zc4rlos <jujuni1000.carlos@gmail.com>
2025-10-28 21:50:43 -0300

    feat(auth): Implementa endpoint de cadastro com envio de e-mail
    Cria o Service e Controller para registrar usuários. O serviço
    salva no repositório, gera o JWT (auto-login) e chama o EmailService
    de forma assíncrona.

--- backend/omegatechapi/src/main/java/com/example/omegatechapi/service/UsuarioService.java
+++ backend/omegatechapi/src/main/java/com/example/omegatechapi/service/UsuarioService.java
@@ -1,9 +1,11 @@
 package com.example.omegatechapi.service;
 
+import com.example.omegatechapi.model.Perfil;
 import com.example.omegatechapi.model.Usuario;
 import com.example.omegatechapi.repository.UsuarioRepository;
 import com.example.omegatechapi.response.AuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +13,13 @@ public class UsuarioService {
 
     @Autowired
     private final UsuarioRepository usuarioRepository;
-    private final JwtService jwtService; //
+    private final JwtService jwtService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private EmailService emailService;
 
     public UsuarioService(UsuarioRepository usuarioRepository, JwtService jwtService) {
         this.usuarioRepository = usuarioRepository;
@@ -27,7 +35,31 @@ public class UsuarioService {
 
         String token = jwtService.gerarToken(usuario);
         return new AuthResponse(usuario.getNome(), token);
+    }
+    public AuthResponse cadastrarNovoUsuario(Usuario request) {
 
+        if (usuarioRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Erro: Este e-mail já está em uso!");
+        }
+
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setNome(request.getNome());
+        novoUsuario.setEmail(request.getEmail());
+        novoUsuario.setSenha(passwordEncoder.encode(request.getSenha()));
+        novoUsuario.setPerfil(Perfil.ROLE_CLIENTE);
+
+        Usuario usuarioSalvo = usuarioRepository.save(novoUsuario);
+
+        String token = jwtService.gerarToken(novoUsuario);
+
+
+        try {
+            emailService.enviarEmailDeBoasVindas(usuarioSalvo.getEmail(), usuarioSalvo.getNome());
+        } catch (Exception e) {
+            System.err.println("Erro ao agendar envio de e-mail: " + e.getMessage());
+        }
+
+        return new AuthResponse(usuarioSalvo.getNome(), token);
     }
 }
 
